20cb5bd8db31ce0cd338e656b08715cb95272de9
Pavel Miskevich <chiki-takipavel@mail.ru>
2024-10-31 22:34:36 +0300

    Add products updating feature

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,29 @@
+﻿namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductRequest(Guid Id, string Name, string Description, string ImageFile,
+    decimal Price, List<string> Categories);
+
+public record UpdateProductResponse(bool IsSuccess);
+
+public class UpdateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<UpdateProductCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<UpdateProductResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("UpdateProduct")
+        .WithDescription("Update Product")
+        .WithSummary("Update Product")
+        .Produces(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound);
+    }
+}
