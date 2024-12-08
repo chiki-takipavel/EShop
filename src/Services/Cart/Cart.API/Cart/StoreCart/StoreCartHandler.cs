fb98a9b555fbb8686f91dadbf335e2c06f37c14b
Pavel Miskevich <55394253+chiki-takipavel@users.noreply.github.com>
2024-12-08 20:09:14 +0300

    Connect Cart microservice to Discount microservice (#4)
    * Rename amount column to percentage
    
    * Implement discount applying in Cart service

--- src/Services/Cart/Cart.API/Cart/StoreCart/StoreCartHandler.cs
+++ src/Services/Cart/Cart.API/Cart/StoreCart/StoreCartHandler.cs
@@ -13,13 +13,27 @@ public class StoreCartCommandValidator : AbstractValidator<StoreCartCommand>
     }
 }
 
-public class StoreCartHandler(ICartRepository repository)
+public class StoreCartHandler(ICartRepository repository,
+    DiscountProtoService.DiscountProtoServiceClient discountProto)
     : ICommandHandler<StoreCartCommand, StoreCartResult>
 {
     public async Task<StoreCartResult> Handle(StoreCartCommand command, CancellationToken cancellationToken)
     {
+        await ApplyDiscount(command.Cart, cancellationToken);
+
         await repository.StoreCart(command.Cart, cancellationToken);
 
         return new StoreCartResult(command.Cart.UserName);
+    }
+
+    private async Task ApplyDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        foreach (var item in cart.Items)
+        {
+            var coupon = await discountProto.GetDiscountAsync(
+                new GetDiscountRequest { ProductName = item.ProductName },
+                cancellationToken: cancellationToken);
+            item.Price *= 1m - (coupon.Percentage / 100m);
+        }
     }
 }
--- src/Services/Discount/Discount.Grpc/Domain/Coupon.cs
+++ src/Services/Discount/Discount.Grpc/Domain/Coupon.cs
@@ -5,5 +5,5 @@ public class Coupon
     public int Id { get; set; }
     public string ProductName { get; set; } = default!;
     public string Description { get; set; } = default!;
-    public int Amount { get; set; }
+    public int Percentage { get; set; }
 }
