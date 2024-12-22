b8bb8e8f83df65485e9f88b56bfcbfbab8719149
Pavel Miskevich <chiki-takipavel@mail.ru>
2024-12-22 21:02:26 +0300

    Add domain models with aggregates, entities, value objects and domain events

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Domain.Abstractions;
+
+public interface IDomainEvent : INotification
+{
+    public Guid EventId => Guid.NewGuid();
+    public Instant OccuredOn => SystemClock.Instance.GetCurrentInstant();
+    public string EventType => GetType().AssemblyQualifiedName!;
+}
