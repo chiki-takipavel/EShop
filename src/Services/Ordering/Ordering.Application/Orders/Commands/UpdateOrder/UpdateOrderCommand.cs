c931d110f1f7eb588fd3f3f29da4afa80f8cc2b1
Pavel Miskevich <chiki-takipavel@mail.ru>
2025-01-22 00:58:46 +0300

    Add order updating

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.Application.Orders.Commands.UpdateOrder;
+
+public record UpdateOrderCommand(OrderDto Order) : ICommand<UpdateOrderResult>;
+
+public record UpdateOrderResult(bool IsSuccess);
+
+internal class UpdateOrderCommandValidator : AbstractValidator<UpdateOrderCommand>
+{
+    public UpdateOrderCommandValidator()
+    {
+        RuleFor(x => x.Order.Id).NotEmpty();
+        RuleFor(x => x.Order.OrderName).NotEmpty().WithName("Name");
+        RuleFor(x => x.Order.CustomerId).NotEmpty();
+    }
+}
