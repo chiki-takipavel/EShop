bf284b2f245bb89ada582ae57c6b3dfbaf37579f
Pavel Miskevich <chiki-takipavel@mail.ru>
2025-02-10 00:24:05 +0300

    Add order queries with pagination

---
+++ src/Services/Ordering/Ordering.Application/Extensions/OrderExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace Ordering.Application.Extensions;
+
+internal static class OrderExtensions
+{
+    internal static IReadOnlyList<OrderDto> ToOrderDtoList(this IEnumerable<Order> orders)
+    {
+        return orders.Select(order => new OrderDto
+        (
+            Id: order.Id.Value,
+            CustomerId: order.CustomerId.Value,
+            OrderName: order.OrderName.Value,
+            ShippingAddress: new AddressDto(order.ShippingAddress.FirstName, order.ShippingAddress.LastName, order.ShippingAddress.EmailAddress!, order.ShippingAddress.AddressLine, order.ShippingAddress.Country, order.ShippingAddress.State, order.ShippingAddress.ZipCode),
+            BillingAddress: new AddressDto(order.BillingAddress.FirstName, order.BillingAddress.LastName, order.BillingAddress.EmailAddress!, order.BillingAddress.AddressLine, order.BillingAddress.Country, order.BillingAddress.State, order.BillingAddress.ZipCode),
+            Payment: new PaymentDto(order.Payment.CardName!, order.Payment.CardNumber, order.Payment.Expiration, order.Payment.CVV, order.Payment.PaymentMethod),
+            Status: order.Status,
+            OrderItems: order.OrderItems.Select(i => new OrderItemDto(i.OrderId.Value, i.ProductId.Value, i.Quantity, i.Price)).ToList()
+        )).ToList();
+    }
+
+    internal static OrderDto ToOrderDto(this Order order)
+    {
+        return new OrderDto
+        (
+            Id: order.Id.Value,
+            CustomerId: order.CustomerId.Value,
+            OrderName: order.OrderName.Value,
+            ShippingAddress: new AddressDto(order.ShippingAddress.FirstName, order.ShippingAddress.LastName, order.ShippingAddress.EmailAddress!, order.ShippingAddress.AddressLine, order.ShippingAddress.Country, order.ShippingAddress.State, order.ShippingAddress.ZipCode),
+            BillingAddress: new AddressDto(order.BillingAddress.FirstName, order.BillingAddress.LastName, order.BillingAddress.EmailAddress!, order.BillingAddress.AddressLine, order.BillingAddress.Country, order.BillingAddress.State, order.BillingAddress.ZipCode),
+            Payment: new PaymentDto(order.Payment.CardName!, order.Payment.CardNumber, order.Payment.Expiration, order.Payment.CVV, order.Payment.PaymentMethod),
+            Status: order.Status,
+            OrderItems: order.OrderItems.Select(i => new OrderItemDto(i.OrderId.Value, i.ProductId.Value, i.Quantity, i.Price)).ToList()
+        );
+    }
+}
