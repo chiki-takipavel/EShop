d29f9fba4ae84443f22ee5e51ea227e81e135dbe
Pavel Miskevich <chiki-takipavel@mail.ru>
2025-03-22 17:46:36 +0300

    Add and configure RabbitMq via MassTransit

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,10 @@
+﻿using NodaTime;
+
+namespace BuildingBlocks.Messaging.Events;
+
+public record IntegrationEvent
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public Instant OcurredOn { get; } = SystemClock.Instance.GetCurrentInstant();
+    public string EventType => GetType().AssemblyQualifiedName!;
+}
